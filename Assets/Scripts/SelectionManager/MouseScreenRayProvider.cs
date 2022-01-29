2e9082755dcec1405bcf3a0ef8771f494767bbaf
raymondbecking <raymondbecking@gmail.com>
2022-01-29 16:21:12 +0100

    Changed raycast to be in front of the camera instead on mousepos

--- Assets/Scripts/SelectionManager/MouseScreenRayProvider.cs
+++ Assets/Scripts/SelectionManager/MouseScreenRayProvider.cs
@@ -4,6 +4,7 @@ public class MouseScreenRayProvider : MonoBehaviour, IRayProvider
 {
     public Ray CreateRay()
     {
-        return Camera.main.ScreenPointToRay(Input.mousePosition);
+        //return Camera.main.ScreenPointToRay(Input.mousePosition);
+        return new Ray(Camera.main.transform.position + Camera.main.transform.forward * 0.01f, Camera.main.transform.forward * 0.01f);
     }
 }
