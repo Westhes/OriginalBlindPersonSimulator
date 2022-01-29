035b7de1d3ad432f61b01c07d5c994476d16edf7
Kaithlyn <kavandeelen@st.noorderpoort.nl>
2022-01-29 23:44:38 +0100

    Main Menu almost done, needs logo

---
+++ Assets/Scripts/GameManager Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
