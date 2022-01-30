25be4fa7a4956b5e23e189c963cc27b8858590c5
Sebas West <s.westheim62@gmail.com>
2022-01-30 16:03:38 +0100

    Fixed an issue where your mouse was still locked when finding the exit. Updated the navmesh.

--- Assets/Scripts/GameManager Scripts/EndScreen.cs
+++ Assets/Scripts/GameManager Scripts/EndScreen.cs
@@ -9,7 +9,7 @@ public class EndScreen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
