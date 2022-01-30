97f5c2195d856373cc0630f305a912e1d9ccbb26
Sebas West <s.westheim62@gmail.com>
2022-01-30 12:17:31 +0100

    Added rock throw collision sound

--- Assets/SonarEchoBounces.cs
+++ Assets/SonarEchoBounces.cs
@@ -10,18 +10,23 @@ public class SonarEchoBounces : MonoBehaviour
     public int echoRepititions = 2;
     public float echoDelayBetweenRepititions = 2f;
 
+    AudioSource audio;
+
     // Start is called before the first frame update
     private void Start()
     {
         if (parent == null) parent = FindObjectOfType<SimpleSonarShader_Parent>();
         settings = GetComponent<SonarSettings>();
+        audio = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (audio) audio.PlayOneShot(audio.clip, collision.impulse.magnitude / 3f);
+
         if (collision.transform.TryGetComponent(out SimpleSonarShader_ExampleCollision _))
         {
-            //parent.StartSonarRing(collision.contacts[0].point, collision.impulse.magnitude / 10.0f * settings.impactMultiplier, settings.SonarColor);
+            
             StartCoroutine(Echo(collision.contacts[0].point, collision.impulse.magnitude / 10.0f * settings.impactMultiplier, echoRepititions));
         }
     }
